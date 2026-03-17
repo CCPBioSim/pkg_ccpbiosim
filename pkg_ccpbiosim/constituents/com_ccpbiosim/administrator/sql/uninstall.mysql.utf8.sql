1b13638ef2e9f6354b35d0fc90fe3cc018424455
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-03-17 16:27:10 +0000

    Update uninstall.mysql.utf8.sql

--- pkg_ccpbiosim/constituents/com_ccpbiosim/administrator/sql/uninstall.mysql.utf8.sql
+++ pkg_ccpbiosim/constituents/com_ccpbiosim/administrator/sql/uninstall.mysql.utf8.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `#__ccpbiosim_core_team`;
-DROP TABLE IF EXISTS `#__ccpbiosim_event_categories`;
-DROP TABLE IF EXISTS `#__ccpbiosim_events`;
-DROP TABLE IF EXISTS `#__ccpbiosim_management_team`;
+-- DROP TABLE IF EXISTS `#__ccpbiosim_core_team`;
+-- DROP TABLE IF EXISTS `#__ccpbiosim_event_categories`;
+-- DROP TABLE IF EXISTS `#__ccpbiosim_events`;
+-- DROP TABLE IF EXISTS `#__ccpbiosim_management_team`;
