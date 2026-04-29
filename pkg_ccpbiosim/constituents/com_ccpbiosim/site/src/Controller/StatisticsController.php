f9771cbf16188c2a39a01c4c9f50e5e18a625296
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-04-29 15:23:01 +0100

    fix controller

--- pkg_ccpbiosim/constituents/com_ccpbiosim/site/src/Controller/StatisticsController.php
+++ pkg_ccpbiosim/constituents/com_ccpbiosim/site/src/Controller/StatisticsController.php
@@ -1,45 +1,25 @@
 <?php
 /**
- * @version    CVS: 1.0.0
- * @package    Com_Ccpbiosim
- * @author     James <james.gebbie@gmail.com>
- * @copyright  2026 James
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @package    com_ccpbiosim
+ * @copyright  2025 CCPBioSim Team
+ * @license    MIT
  */
 
 namespace Ccpbiosim\Component\Ccpbiosim\Site\Controller;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\SiteApplication;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Multilanguage;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\Controller\FormController;
-use Joomla\CMS\Router\Route;
-use Joomla\CMS\Uri\Uri;
-use Joomla\Utilities\ArrayHelper;
+use Joomla\CMS\MVC\Controller\BaseController;
 
 /**
- * Statistics class.
- *
- * @since  1.0.0
+ * Statistics controller - read-only view, no form handling needed.
  */
-class StatisticsController extends FormController
+class StatisticsController extends BaseController
 {
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @param   string  $name    The model name. Optional.
-	 * @param   string  $prefix  The class prefix. Optional
-	 * @param   array   $config  Configuration array for model. Optional
-	 *
-	 * @return  object	The model
-	 *
-	 * @since   1.0.0
-	 */
-	public function getModel($name = 'Statistics', $prefix = 'Site', $config = array())
-	{
-		return parent::getModel($name, $prefix, array('ignore_request' => true));
-	}
+    /**
+     * The default view.
+     *
+     * @var string
+     */
+    protected $default_view = 'statistics';
 }
